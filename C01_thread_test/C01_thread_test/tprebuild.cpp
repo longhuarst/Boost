2bdc38faa5a9a43b9967094b408aa5d5c0d698f9
longhuarst <841105197@qq.com>
2016-11-28 23:32:48 +0800

    add tprebuild.cpp

--- C01_thread_test/C01_thread_test/tprebuild.cpp
+++ C01_thread_test/C01_thread_test/tprebuild.cpp
@@ -1,3 +1,14 @@
 //tprebuild.cpp
-#define BOOST_DATE_TIME_SOURCE
+#define BOOST_DATE_TIME_SOURCE	//thread库需要使用date_time库
 #define BOOST_THREAD_NO_LIB
+#include <boost/thread.hpp>
+
+#ifdef _MSC_VER	//windows 系统的线程
+extern "C" void tss_cleanup_implemented(void) {} //一个必须的函数
+#include <libs/thread/src/win32/thread.cpp>
+#include <libs/thread/src/win32/tss_dll.cpp>
+#include <libs/thread/src/win32/tss_pe.cpp>
+#else	//unix 系统相关的实现文件
+#include <libs/thread/src/pthread/thread.cpp>
+#include <libs/thread/src/pthread/once.cpp>
+#endif // _MSC_VER
