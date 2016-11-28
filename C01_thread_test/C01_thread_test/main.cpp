b5a8516546358683fd356a3c21035682d492df27
longhuarst <841105197@qq.com>
2016-11-28 23:59:05 +0800

    atom_int

--- C01_thread_test/C01_thread_test/main.cpp
+++ C01_thread_test/C01_thread_test/main.cpp
@@ -6,6 +6,27 @@
 
 using namespace std;
 
+namespace boost {
+	template<typename T>
+	class basic_atom :noncopyable
+	{
+	public:
+		basic_atom(T x = T()) :n(x) {}
+		T operator++()
+		{
+			mutex_t::scoped_lock lock(mu);
+			return ++n;
+		}
+		operator T() { return n; }
+	protected:
+	private:
+		T n;
+		typedef mutex mutex_t;
+		mutex_t mu;
+	};
+}
+
+
 
 int main(int argc, char *argv[])
 {
@@ -29,6 +50,11 @@ int main(int argc, char *argv[])
 	boost::mutex::scoped_lock lock(mu2); //使用RAII型的lock_guard
 	cout << "some operations" << endl;
 
+	system("pause");
+
+	typdef boost::basic_atom<int> atom_int;
+	atom_int x;
+	cout << ++x;
 	system("pause");
 	
 	return 0;
